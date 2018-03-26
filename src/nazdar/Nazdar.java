777160e89c6bf0b0c3a5ff6f85dca46fe8244545
tomas.koten <tomas.koten@W10413.sps-cl.local>
2018-03-26 13:20:50 +0200

    První commit

---
+++ src/nazdar/Nazdar.java
@@ -0,0 +1,14 @@
+
+package nazdar;
+public class Nazdar {
+
+    
+    public static void main(String[] args) {
+        System.err.println("Nazdar Světe");
+        for (int i = 0; i < 10; i++) {
+            System.err.println(i);
+                
+            }
+    }
+    
+}
